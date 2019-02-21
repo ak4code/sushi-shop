eabbd34041c3a850757e37b62bcc64b263c0903b
haritonov.aka@gmail.com <03Feb1987>
2019-02-21 10:25:45 +0300

    cart checkout filter

--- shop/admin.py
+++ shop/admin.py
@@ -7,6 +7,29 @@ class OrderAdmin(admin.ModelAdmin):
     list_display = ('__str__', 'name', 'phone', 'address', 'person', 'created')
 
 
+class CartCheckoutListFilter(admin.SimpleListFilter):
+    title = 'Заказ'
+
+    parameter_name = 'order'
+
+    def lookups(self, request, model_admin):
+
+        return (
+            ('c', 'Оформленные'),
+            ('n', 'Не оформленные'),
+        )
+
+    def queryset(self, request, queryset):
+
+        if self.value() == 'c':
+            return queryset.filter(order__isnull=False)
+        if self.value() == 'n':
+            return queryset.filter(order__isnull=True)
+
+
 @admin.register(Cart)
 class CartAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('__str__', 'order', 'created')
+    list_filter = (
+        CartCheckoutListFilter,
+    )
