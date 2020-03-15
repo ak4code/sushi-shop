0ca969cac90ede7e56cb185111ead90bc91c446b
ak4code <haritonov.aka@gmail.com>
2020-03-15 13:21:30 +0300

    feat: tg bot check updates

--- sushishop/tgbot.py
+++ sushishop/tgbot.py
@@ -6,3 +6,9 @@ bot = telebot.TeleBot(config('BOT_TOKEN'))
 
 def tg_send_order(message):
     bot.send_message(215750267, message)
+
+
+def check_new_updates():
+    updates = bot.get_updates()
+    for update in updates:
+        print(update)
