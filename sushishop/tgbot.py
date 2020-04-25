8129387684695cdcea332d2bba4e59447f3f29ac
akweb <haritonov.aka@gmail.com>
2020-04-25 22:13:15 +0300

    feat: tg bot send order dmitry

--- sushishop/tgbot.py
+++ sushishop/tgbot.py
@@ -6,6 +6,7 @@ bot = telebot.TeleBot(config('BOT_TOKEN'))
 
 def tg_send_order(message):
     bot.send_message(897458587, message)
+    bot.send_message(672581035, message)
 
 
 def check_new_updates():
